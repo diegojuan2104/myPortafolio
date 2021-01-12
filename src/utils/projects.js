7dc4afe0e8da77b8969ba36a09d2c6e797a1046e
diegojuan2104 <diegojuan2104@gmail.com>
2021-01-12 16:28:20 -0500

    Some projects added

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,5 +1,9 @@
 
 import img_veterinaria from "../images/projects/Veterinaria_admin.png"
+import img_bebidas from "../images/projects/Buscador_de_recetas.png"
+import img_portafolio from "../images/projects/Portafolio.png"
+
+
 
 const projects = [
     {
@@ -9,8 +13,20 @@ const projects = [
         "img": img_veterinaria,
         "tags": ["React", "Sass","JavaScript","HTML"]
     },
-    
-
+    {
+        "title":"Buscador de bebidas",
+        "code":"https://github.com/diegojuan2104/sistema_citas_react_sass",
+        "url":"https://buscadorrecetas.netlify.app/",
+        "img": img_bebidas,
+        "tags": ["React", "Css","JavaScript","HTML","Boostrap 4"]
+    },
+    {
+        "title":"Portafolio",
+        "code":"https://github.com/diegojuan2104/sistema_citas_react_sass",
+        "url":"https://juanmejia.netlify.app/",
+        "img": img_portafolio,
+        "tags": ["React", "Css","JavaScript","HTML","Boostrap 4"]
+    },
 ]
 
 export { projects };
